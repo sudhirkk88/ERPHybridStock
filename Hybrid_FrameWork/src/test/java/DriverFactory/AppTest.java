5dec57444a057ba55651d64958f463d52af756f2
USER <USER@LAPTOP-L94CPCHI>
2024-07-09 20:10:18 +0530

    HybridFramework Structure

---
+++ Hybrid_FrameWork/src/test/java/DriverFactory/AppTest.java
@@ -0,0 +1,12 @@
+package DriverFactory;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+@Test
+public void kickStrat() throws Throwable
+{
+	DriverScript ds = new DriverScript();
+	ds.startTest();
+}
+}
